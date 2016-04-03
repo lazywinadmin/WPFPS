e59b38347231da1dffcaa2637e88a35ccdac48fc
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2016-04-02 21:25:30 -0400

    Update Function

---
+++ WPFPS/Public/Set-WPFForm.ps1
@@ -0,0 +1,24 @@
+﻿function Set-WPFForm
+{
+	PARAM (
+		[System.Windows.Window]$Form,
+		[String]$Title
+	)
+	BEGIN 
+	{
+		Add-Type –assemblyName PresentationFramework
+		Add-Type –assemblyName PresentationCore
+		Add-Type –assemblyName WindowsBase
+	}
+	PROCESS
+	{
+		IF ($PSBoundParameters['Title'])
+		{
+			$Form.Title = $Title
+		}
+	}
+	END
+	{
+		
+	}
+}
